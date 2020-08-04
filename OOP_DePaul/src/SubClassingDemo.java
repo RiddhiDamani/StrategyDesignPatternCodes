da6472106736a56a928755491110fe0f9dec3cc6
Riddhi Damani <riddhidamani90@gmail.com>
2020-08-04 12:38:07 -0500

    Constructors Demo

---
+++ OOP_DePaul/src/SubClassingDemo.java
@@ -0,0 +1,23 @@
+class C {
+	int i;
+	public C() {
+		System.out.println(i);
+		System.out.println("C");
+		i = 27;
+		System.out.println(i);
+	}
+}
+
+class D extends C {
+	public D(){
+		super();
+		System.out.println("D");
+	}
+}
+
+public class SubClassingDemo {
+	public static void main(String[] args) {
+		C c = new C();
+		c = new D();
+	}
+}
